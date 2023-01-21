01b1ecb0cdfa061452b0532d2eb41403125b377d
Caio Giffoni <caio_cgf@hotmail.com>
2023-01-21 16:49:56 -0300

    prisma config

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get("/", () => {
-  return "Banana";
+app.register(cors);
+
+app.get("/hello", async () => {
+  const habits = await prisma.habit.findMany();
+
+  return habits;
 });
 
-app.listen({
-  port: 3333,
-});
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server running");
+  });
